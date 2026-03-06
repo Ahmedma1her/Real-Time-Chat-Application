2f80322ab97ad6018be0846c017bde9d90ebe453
Ahmed Maher <AhmedMa1her@gmail.com>
2026-03-06 21:37:47 +0200

    phase 1// the installing and creation of the Database

---
+++ app.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+
+// DB
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
+   
+  }
+};
+connectDB();
+
+// middlewares
+app.use(cors());
+// added cors as it enables the backend to receive requests from different origins (frontend server)// still need to search more about it but understood the concept
+app.use(express.json());
+
+
+
+
+// Routes
+const router=require("./routes/authRoutes")
+app.use("/api/auth",router );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
